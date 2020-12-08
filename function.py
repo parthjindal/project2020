43850a5a41c56ca85376c235e0615fa5a5e419cf
Parth <pmjindal@gmail.com>
2020-12-09 04:49:21 +0530

    Set retain_grad True

--- function.py
+++ function.py
@@ -1,4 +1,12 @@
 def train():
+    #checkout https://stackoverflow.com/questions/46774641/what-does-the-parameter-retain-graph-mean-in-the-variables-backward-method
+    '''
+    # suppose you first back-propagate loss1, then loss2 (you can also do the reverse)
+    loss1.backward(retain_graph=True)
+    loss2.backward() # now the graph is freed, and next process of batch gradient descent is ready
+    optimizer.step() # update the network parameters
+    '''
+
     pass
 
 def validate():
