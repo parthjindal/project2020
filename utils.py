3c1afedf334653eb1c2388b47f402c8bb194707c
Parth <pmjindal@gmail.com>
2020-12-09 15:36:21 +0530

    Add Logger Class

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import torch
 from torch import optim
 from config import _C as cfg
+from torch.utils.tensorboard import SummaryWriter
+import os
 
 
 def get_optimizer(model, cfg):
@@ -24,9 +26,30 @@ def get_optimizer(model, cfg):
     return optimizer
 
 
-def create_logger():
-    # TODO
-    pass
+class Logger:
+    def __init__(self, log_dir, summary_writer=None):
+        self._log_dir = log_dir
+        print('########################')
+        print('Logging outputs to ', log_dir)
+        print('########################')
+        self._summ_writer = SummaryWriter(log_dir, flush_secs=1, max_queue=1)
+
+    def log_scalar(self, scalar, name, step_):
+        self._summ_writer.add_scalar('{}'.format(name), scalar, step_)
+
+    def log_scalars(self, scalar_dict, group_name, step):
+        """Will log all scalars in the same plot."""
+        self._summ_writer.add_scalars(
+            '{}_{}'.format(group_name), scalar_dict, step)
+
+    def log_video(self, video_frames, name, step, fps=10):
+        assert len(
+            video_frames.shape) == 5, "Need [N, T, C, H, W] input tensor for video logging!"
+        self._summ_writer.add_video('{}'.format(
+            name), video_frames, step, fps=fps)
+
+    def flush(self):
+        self._summ_writer.flush()
 
 
 def save_checkpoint(states, is_best, output_dir=cfg.MODEL.DIR,
