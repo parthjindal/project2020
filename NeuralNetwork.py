07d805b0cb9d5bad9f6302a3df103ff3dc190f09
Abhishek Gandhi <abhigandhi29@gmail.com>
2020-12-09 14:07:00 +0530

    adding additional outputs for training

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -10,11 +10,6 @@ class BuildBlock(nn.Module):
     def __init__(self, feature_length=None, num_filters=1, filter_length=1, stride=(1, 1, 1), eps=1e-5, momentum=0, **kwargs):
         super(BuildBlock, self).__init__()
 
-        if type(stride) is int:  # Stride for All 3 Subblocks
-            stride = (stride,)+2*(1,)
-        elif type(stride) is tuple:
-            stride = stride + (3-len(stride))*(1,)
-
         self.block = nn.Sequential(OrderedDict([
             ('blockA', ResTCN(feature_length, num_filters,
                               filter_length, stride[0], eps, momentum, **kwargs)),
@@ -45,7 +40,10 @@ class Network(nn.Module):
         self.pad = nn.ConstantPad1d(padding, value=0)
         self.conv1 = nn.Conv1d(in_channels=feature_length,
                                out_channels=cfg.BLOCK1.NUM_FILTERS, kernel_size=cfg.FILTER_LENGTH, stride=1)
-
+        self.linear1 = nn.linear(in_channels=cfg.BLOCK1.NUM_FILTERS,out_channels=50)
+        self.linear2 = nn.linear(in_channels=cfg.BLOCK2.NUM_FILTERS,out_channels=50)
+        self.linear3 = nn.linear(in_channels=cfg.BLOCK3.NUM_FILTERS,out_channels=50)
+        self.linear4 = nn.linear(in_channels=cfg.BLOCK4.NUM_FILTERS,out_channels=50)
         self.Block1 = BuildBlock(
             cfg.BLOCK1.NUM_FILTERS, cfg.BLOCK1.NUM_FILTERS, cfg.FILTER_LENGTH, cfg.BLOCK1.STRIDE)
         self.Block2 = BuildBlock(
@@ -59,8 +57,20 @@ class Network(nn.Module):
         ########TODO: ADD FCs,GLOBAL_AVG_POOLINGs,CONCATENATION,CHANGE RETURN TO (PRED1,PRED2,PRED3,X)#####
         x = self.pad(x)
         x = self.conv1(x)
-        x = self.Block1(x)
-        x = self.Block2(x)
-        x = self.Block3(x)
-        x = self.Block4(x)
-        return x
+        x1 = self.Block1(x)
+        x = x + x1
+        x1 = x.mean(dim=(2))
+        x1 = self.linear1(x1)
+        x2 = self.Block2(x)
+        x = x + x2
+        x2 = x.mean(dim=(2))
+        x2 = self.linear2(x2)
+        x3 = self.Block3(x)
+        x = x + x3
+        x3 = x.mean(dim=(2))
+        x3 = self.linear3(x3)
+        x4 = self.Block4(x)
+        x = x + x4
+        x4 = x.mean(dim=(2))
+        x4 = self.linear3(x4)
+        return x1,x2,x3,x4
